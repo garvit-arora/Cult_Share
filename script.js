3320f0c9f2f0886ced6e7266581770bd2cdc7e95
Garvit Arora <garvit.university@gmail.com>
2025-06-20 10:41:29 +0530

    fixing login page linking

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function skillpage(){
     event.preventDefault();
     window.location.href = "skills.html";
 }
+function loginpage(){
+    event.preventDefault();
+    window.location.href = "login.html";
+}
