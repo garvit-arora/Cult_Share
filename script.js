d090ad885dbcd84a70c4508faba57b4ecf63398c
unknown <jainpallavi.delhi@gmail.com>
2025-06-20 17:49:05 +0530

    pricing fixed

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ function loginpage(){
     event.preventDefault();
     window.location.href = "login.html";
 }
+document.getElementById("back-button").addEventListener("click", function() {
+    window.history.back();
+});
